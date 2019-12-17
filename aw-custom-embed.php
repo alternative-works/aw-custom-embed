20f6865429f3c20bdbc59836f72339fd519dd7dd
alternative-works <takanashi.blog@gmail.com>
2019-12-17 23:04:38 +0900

    Github更新通知が来ない不具合を再修正

--- aw-custom-embed.php
+++ aw-custom-embed.php
@@ -4,14 +4,27 @@ Plugin Name: AW Custom Embed
 Plugin URI: https://careru.jp/
 Description: Embedをカスタマイズしたもの
 Author: Alternative Works
-Version: 0.0.3
+Version: 0.0.2
 Author URI: https://careru.jp/
 */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require_once( dirname( __FILE__ ) . '/update-checker/update-checker.php');
+if(!defined('UPDATE_CHECKER_LOADED')){
+    require_once( dirname( __FILE__ ) . '/update-checker/vendor/autoload.php' );
+}
+define('UPDATE_CHECKER_LOADED',true);
+
+add_action( 'init', function(){
+        $plugin_slug = plugin_basename( __FILE__ ); // e.g. `hello/hello.php`.
+        $gh_user = 'alternative-works';                      // The user name of GitHub.
+        $gh_repo = 'aw-custom-embed';       // The repository name of your plugin.
+
+        // Activate automatic update.
+        new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
+    }
+);
 require_once( dirname( __FILE__ ) . '/common/main.php' );
 require_once( dirname( __FILE__ ) . '/common/codepen.php' );
 require_once( dirname( __FILE__ ) . '/common/chrome-webstore.php' );
